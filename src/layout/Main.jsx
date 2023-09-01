1e8afa78d0d1b45e46fc954f280d7f389cda4cd5
mrdltn <mrdltn@hotmail.com>
2023-09-01 12:06:16 +0700

    fixed bugs

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,7 +12,7 @@ class Main extends React.Component {
     };
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=putin`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=putin`)
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
@@ -22,7 +22,7 @@ class Main extends React.Component {
     searchMovies = (str, type = 'all') => {
         this.setState({ loading: true });
         fetch(
-            `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
                 type !== 'all' ? `&type=${type}` : ''
             }`
         )
